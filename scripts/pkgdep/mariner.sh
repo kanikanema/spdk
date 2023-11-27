3b6cbe99e020b564d1b22e7e7cd2724928c00cbf
Kanika Nema <kanikanema@microsoft.com>
2023-11-27 04:40:18 +0000

    Fixes and cleanup

--- scripts/pkgdep/mariner.sh
+++ scripts/pkgdep/mariner.sh
@@ -3,46 +3,46 @@
 #  SPDX-License-Identifier: BSD-3-Clause
 #  All rights reserved.
 
-is_repo() { tdnf repolist --all | grep -q "^$1"; }
+is_repo() { sudo tdnf repolist --all | grep -q "^$1"; }
 
 additional_dependencies() {
 	# Additional dependencies for SPDK CLI
-	tdnf install -y python3-pexpect
+	sudo tdnf install -y python3-pexpect
 	# Additional dependencies for ISA-L used in compression
-	tdnf install -y help2man
+	sudo tdnf install -y help2man
 	# Additional dependencies for DPDK
 	if ! [[ "$(uname -m)" = "aarch64" ]]; then
-		tdnf install -y nasm
+		sudo tdnf install -y nasm
 	fi
-	tdnf install -y libnuma-devel
+	sudo tdnf install -y libnuma-devel
 	# Additional dependencies for USDT
-	tdnf install -y systemtap-sdt-devel
+	sudo tdnf install -y systemtap-sdt-devel
 	if [[ $INSTALL_DEV_TOOLS == "true" ]]; then
 		# Tools for developers
 		devtool_pkgs=(git sg3_utils pciutils bash-completion ruby-devel)
 		devtool_pkgs+=(gcovr python3-pycodestyle)
-		tdnf install -y "${devtool_pkgs[@]}"
+		sudo tdnf install -y "${devtool_pkgs[@]}"
 	fi
 	if [[ $INSTALL_PMEM == "true" ]]; then
 		# Additional dependencies for building pmem based backends
-		tdnf install -y libpmemobj-devel || true
+		sudo tdnf install -y libpmemobj-devel || true
 	fi
 	if [[ $INSTALL_FUSE == "true" ]]; then
 		# Additional dependencies for FUSE and NVMe-CUSE
-		tdnf install -y fuse3-devel
+		sudo tdnf install -y fuse3-devel
 	fi
 	if [[ $INSTALL_RBD == "true" ]]; then
 		# Additional dependencies for RBD bdev in NVMe over Fabrics
-		tdnf install -y librados-devel librbd-devel
+		sudo tdnf install -y librados-devel librbd-devel
 	fi
 	if [[ $INSTALL_RDMA == "true" ]]; then
 		# Additional dependencies for RDMA transport in NVMe over Fabrics
-		tdnf install -y libibverbs librdmacm
+		sudo tdnf install -y libibverbs librdmacm
 	fi
 	if [[ $INSTALL_DOCS == "true" ]]; then
 		# Additional dependencies for building docs
-		tdnf install -y mscgen || echo "Warning: couldn't install mscgen via tdnf. Please install mscgen manually."
-		tdnf install -y doxygen graphviz
+		sudo tdnf install -y mscgen || echo "Warning: couldn't install mscgen via sudo tdnf. Please install mscgen manually."
+		sudo tdnf install -y doxygen graphviz
 	fi
 	if [[ $INSTALL_DAOS == "true" ]]; then
 		echo "Unsupported. Skipping installation of DAOS bdev dependencies."
@@ -50,38 +50,31 @@ additional_dependencies() {
 	# Additional dependencies for Avahi
 	if [[ $INSTALL_AVAHI == "true" ]]; then
 		# Additional dependencies for Avahi
-		tdnf install -y avahi-devel
+		sudo tdnf install -y avahi-devel
 	fi
 }
 
-tdnf install -y ca-certificates build-essential
-tdnf install -y CUnit-devel \
-   clang \
-   clang-devel \
-   cmake \
-   json-c-devel \
-   libaio-devel \
-   libcmocka-devel \
-   libiscsi-devel \
-   libuuid-devel \
-   meson \
-   ncurses-devel \
-   ninja-build \
-   openssl-devel \
-   procps-ng \
-   python \
-   python3-devel \
-   python3-pip \
-   tar \
-   unzip \
-# Minimal install
-# workaround for arm: ninja fails with dep on skbuild python module
-#if [ "$(uname -m)" = "aarch64" ]; then
-#	pip3 install scikit-build
-#fi
+sudo tdnf install -y ca-certificates build-essential
+sudo tdnf install -y CUnit-devel \
+	clang \
+	clang-devel \
+	cmake \
+	json-c-devel \
+	libaio-devel \
+	libcmocka-devel \
+	libiscsi-devel \
+	libuuid-devel \
+	ncurses-devel \
+	openssl-devel \
+	procps-ng \
+	python \
+	python3-devel \
+	python3-pip \
+	tar \
+	unzip \
 
 if [[ ! -e /usr/bin/python ]]; then
-	ln -s /usr/bin/python3 /usr/bin/python
+	sudo ln -s /usr/bin/python3 /usr/bin/python
 fi
 pip3 install meson
 pip3 install ninja
